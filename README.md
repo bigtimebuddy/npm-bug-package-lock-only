d812c75807de756fdc9db4a85625257c92cf093f
Matt Karl <mattkarl@netflix.com>
2021-10-29 13:32:33 -0400

    Simplify

---
+++ README.md
@@ -0,0 +1,15 @@
+# To Reproduce
+
+After bump versions in the repository, doing `npm install --package-lock-only`.
+
+## Successful
+
+```bash
+npm i npm@7.20.2 -g && npm test
+```
+
+## Failed
+
+```bash
+npm i npm@8 -g && npm test
+```
--- bump.js
+++ bump.js
@@ -4,7 +4,7 @@ const readJson = async file => JSON.parse(await readFile(file, 'utf8'));
 const writeJson = (file, data) => writeFile(file, JSON.stringify(data, null, 2));
 
 async function main() {
-    const files = ['packages/tools-a', 'packages/package-a', 'packages/package-b'];
+    const files = ['packages/tools-a', 'packages/package-a'];
     for (const file of files) {
         const pkg = await readJson(`${file}/package.json`);
         pkg.version = '2.0.0';
