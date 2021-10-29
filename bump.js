162bab9e792eb2983d3a715055ad66e04b84a104
Matt Karl <mattkarl@netflix.com>
2021-10-29 13:14:19 -0400

    Bump script

---
+++ bump.js
@@ -0,0 +1,18 @@
+const { promises: { readFile, writeFile } } = require('fs');
+
+const readJson = async file => JSON.parse(await readFile(file, 'utf8'));
+const writeJson = (file, data) => writeFile(file, JSON.stringify(data, null, 2));
+
+async function main() {
+    const files = ['packages/internal-a', 'packages/package-a', 'packages/package-b'];
+    for (const file of files) {
+        const pkg = await readJson(`${file}/package.json`);
+        pkg.version = '2.0.0';
+        if (pkg.devDependencies) {
+            pkg.devDependencies['@internal/a'] = '2.0.0';
+        }
+        await writeJson(`${file}/package.json`, pkg);
+    }
+}
+
+main();
